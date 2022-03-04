72a2e85792769908b8994d00d59d7350935ae25b
dhavaseelanprathiba <100896580+dhavaseelanprathiba@users.noreply.github.com>
2022-03-04 11:42:52 +0530

    Add files via upload

---
+++ js calculator/js1/script.js
@@ -0,0 +1,24 @@
+function toFunction() 
+{
+  document.getElementById("demo1").style.fontSize = "35px"; 
+  document.getElementById("demo1").style.color = "red";
+  document.getElementById("demo1").style.backgroundColor = "yellow"; 
+  
+  document.getElementById("demo1").style.textAlign = "center";
+  document.getElementById("demo1").style.padding = "30px";
+  document.getElementById("demo1").style.borderRadius = "20px";
+  }
+ /*
+  var
+  let
+  const
+   */
+var c=50;
+console.log(c);
+
+var c=60;
+console.log(c);
+
+const student={"name":"Dhavasi","age":25}
+console.table("student");
+
